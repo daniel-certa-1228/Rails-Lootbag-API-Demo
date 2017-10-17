299bbbdbffad01e1a299b746ebbfbb5d4c5e6fbb
daniel-certa-1228 <mfdancerta@gmail.com>
2017-10-17 15:10:10 -0500

    class exercise

--- app/controllers/children_controller.rb
+++ app/controllers/children_controller.rb
@@ -5,8 +5,26 @@ class ChildrenController < ApplicationController
 		json_response(@children)
 	end
 
-	# def create
-	# 	@child = Child.create!(child_params)
-	# end
+	def create
+		@child = Child.create(params.permit(:name, :delivered))
+		json_response(@child)
+	end
 
+	def show
+		@child = Child.find(params[:id])
+		json_response(@child)
+	end
+
+	def update
+		@child = Child.find(params[:id])
+		@child.update(params.permit(:name, :delivered))
+		json_response(@child)
+	end
+
+	def destroy
+		@child = Child.find(params[:id])
+		@child.destroy
+		# @children = Child.all
+		# json_response(@children)
+	end
 end
